08ba7805a6c9f6fec326f02059f5728ce8c62de5
Leah King <dancingheart714@yahoo.com>
2021-05-12 16:59:14 -0400

    middleware tasks 2 & 3

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -25,24 +25,59 @@ const checkSchemeId = async (req, res, next) => {
 
 /*
   If `scheme_name` is missing, empty string or not a string:
-
   status 400
   {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {};
+const validateScheme = (req, res, next) => {
+  if (
+    !req.body.scheme_name ||
+    !req.body.scheme_name !== 'string' ||
+    !req.body.scheme_name === ''
+  ) {
+    res.status(400).json({
+      message: 'invalid scheme_name',
+    });
+  }
+  next();
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
   if `step_number` is not a number or is smaller than one:
-
   status 400
   {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {};
+const validateStep = (req, res, next) => {
+  return (req, res, next) => {
+    if (!req.body.instructions) {
+      return res.status(400).json({
+        message: 'invalid step',
+      });
+    } else {
+      if (
+        typeof req.body.instructions !== 'string' ||
+        req.body.instructions === ''
+      ) {
+        return res.status(400).json({
+          message: 'invalid step',
+        });
+      } else if (
+        typeof req.body.step_number !== 'number' ||
+        req.body.step_number < 1
+      ) {
+        return res.status(400).json({
+          message: 'invalid step',
+        });
+      } else {
+        next();
+      }
+    }
+  };
+};
 
 module.exports = {
   checkSchemeId,
