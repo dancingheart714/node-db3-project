8d0d5d249a8e53375e8565ce327a125ac210593a
Leah King <dancingheart714@yahoo.com>
2021-05-12 17:58:14 -0400

    last sql query done

--- queries.sql
+++ queries.sql
@@ -3,20 +3,24 @@
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 SELECT  
     productname, categoryname
-FROM  product as p
-JOIN  category as c on p.categoryid = c.id;
+FROM  product AS p
+JOIN  category AS c ON p.categoryid = c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT
     o.id, s.companyname
-FROM  [order] as o
-JOIN  shipper as s on s.id = o.shipvia
+FROM  [order] AS o
+JOIN  shipper AS s ON s.id = o.shipvia
 WHERE o.orderdate <= "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT p.productname, od.quantity
-FROM  product as p
-JOIN  orderdetail as od on od.productid = p.id
+FROM  product AS p
+JOIN  orderdetail AS od ON od.productid = p.id
 GROUP BY p.productname;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT o.id AS orderid, c.companyname AS customer_company_name, e.lastname AS employee_last_name
+FROM  [order] AS o
+JOIN  customer AS c ON c.id = o.customerid
+JOIN  employee AS e ON e.id = o.employeeid
