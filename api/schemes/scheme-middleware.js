29c615d9a2645154d4b5e525852905d16f920504
Leah King <dancingheart714@yahoo.com>
2021-05-13 10:33:18 -0400

    did some post review cleanup

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,82 +1,53 @@
-const Scheme = require('./scheme-model');
+const { Router } = require('express');
+const db = require('../../data/db-config');
 
-/*
-  If `scheme_id` does not exist in the database:
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = async (req, res, next) => {
   try {
-    const schemeId = await Scheme.findById(req.params.scheme_id);
-    if (schemeId) {
-      req.scheme = schemeId;
-      next();
-    } else {
-      res.status(404).json({
-        message: `scheme with scheme_id ${schemeId.id} not found`,
+    const existing = await db('schemes')
+      .where('scheme_id', req.params.scheme.id)
+      .first();
+
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       });
+    } else {
+      next();
     }
   } catch (err) {
     next(err);
   }
 };
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
   if (
-    !req.body.scheme_name ||
-    !req.body.scheme_name !== 'string' ||
-    !req.body.scheme_name === ''
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
   ) {
-    res.status(400).json({
-      message: 'invalid scheme_name',
-    });
+    next({ status: 400, message: 'invalid scheme_name' });
+  } else {
+    next();
   }
-  next();
 };
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-  return (req, res, next) => {
-    if (!req.body.instructions) {
-      return res.status(400).json({
-        message: 'invalid step',
-      });
-    } else {
-      if (
-        typeof req.body.instructions !== 'string' ||
-        req.body.instructions === ''
-      ) {
-        return res.status(400).json({
-          message: 'invalid step',
-        });
-      } else if (
-        typeof req.body.step_number !== 'number' ||
-        req.body.step_number < 1
-      ) {
-        return res.status(400).json({
-          message: 'invalid step',
-        });
-      } else {
-        next();
-      }
-    }
-  };
+  const { instructions, step_number } = req.body;
+
+  if (
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = { status: 400, message: 'invalid step' };
+    next(error);
+  } else {
+    next();
+  }
 };
 
 module.exports = {
